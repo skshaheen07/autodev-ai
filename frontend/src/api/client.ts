6bdf10d17baf39ecce8b28becf88b1f55f0978eb
Shaik Shaheen Tabassum <shaikshaheentabassum1@gmail.com>
2026-08-02 06:46:29 +0530

    Make API URL configurable for deployment

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,9 @@
 ﻿import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1'
+
 const client = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL,
 })
 
 client.interceptors.request.use((config) => {
